bd025d736a49bad14458333092a6bfb6e0cf95ad
Darpan Madlani <Darpan@proclink.com>
2021-12-09 20:49:28 +0530

    find max number from integers

--- src/com/maxnumber/MaximumNumber.java
+++ src/com/maxnumber/MaximumNumber.java
@@ -2,6 +2,24 @@ package com.maxnumber;
 
 public class MaximumNumber {
 
-	
-	
+	public int findMaxNumber(int x, int y, int z) {
+		if (x > y && x > z) {
+			return x;
+		}
+		if (y > x && y > z) {
+			return y;
+		}
+		if (z > y && z > x) {
+			return z;
+		}
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		int intX = 45, intY = 51, intZ = 97;
+		
+		MaximumNumber maxInt = new MaximumNumber();
+		System.out.println(maxInt.findMaxNumber(intX, intY, intZ));
+	}
+
 }
